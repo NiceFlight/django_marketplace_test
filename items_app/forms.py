b5c753a3478735a0820c090bf0493da646bfa168
NiceeFlight <stray.zz@gmail.com>
2025-01-21 16:26:15 +0800

    initial commit

---
+++ items_app/forms.py
@@ -0,0 +1,61 @@
+from django import forms
+
+from .models import Item
+
+INPUT_CLASS = 'w-full py-4 px-6 rounded-xl border'
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['category', 'name', 'description', 'price', 'image']
+        # labels = {
+        #     'category': 'Category',
+        #     'name': 'Item Name',
+        #     'description': 'Description',
+        #     'price': 'Price',
+        #     'image': 'Image',
+        # }
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASS
+                }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASS
+                }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASS
+                }),
+            'price': forms.NumberInput(attrs={
+                'class': INPUT_CLASS
+                }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASS
+                }),
+        }
+
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['name', 'description', 'price', 'image', 'is_sold']
+        # labels = {
+        #     'category': 'Category',
+        #     'name': 'Item Name',
+        #     'description': 'Description',
+        #     'price': 'Price',
+        #     'image': 'Image',
+        # }
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASS
+                }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASS
+                }),
+            'price': forms.NumberInput(attrs={
+                'class': INPUT_CLASS
+                }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASS
+                }),
+        }
